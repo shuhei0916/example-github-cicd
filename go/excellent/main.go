12a64f9f464528b7147baeb40e9d0ce2d0d798df
shuhei0916 <shuheiito.stc@gmail.com>
2024-08-24 12:59:02 +0900

    Add go-test.yml, and go files

---
+++ go/excellent/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func EvenOrOdd(number int) string {
+	if number%2 == 0 {
+		return "even"
+	} else {
+		return "odd"
+	}
+}
+
+func main() {
+	fmt.Println(1 + 2)
+	fmt.Println(EvenOrOdd(57))
+}
